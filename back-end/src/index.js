35f61786133891510e9a30ee0a50298c95c7bc88
Kirill Epelbaum <150293425+kepelbaum@users.noreply.github.com>
2024-07-26 21:01:10 -0400

    fix: fixed jpg link

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(async (req, res, next) => {
+app.use(async (req, res, next) => { 
   req.context = {
     models, 
   };
@@ -186,7 +186,7 @@ const createUsersWithMessages = async () => {
     title: 'Sunset',
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     user: user1.id,
-    image_url: 'https://images.unsplash.com/photo-1720440931331-bdc0e7af2045?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    image_url: 'https://images.unsplash.com/photo-1481988535861-271139e06469?q=80&w=2690&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     ifPublished: true,
   })
 
